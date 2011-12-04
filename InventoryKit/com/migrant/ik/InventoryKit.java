d679a92042be6148ad686353fa513b6d17d4ba9f
Aubrey <aubrey.goodman@gmail.com>
2011-12-04 15:43:52 -0500

    added minimal persistence support for products

--- InventoryKit/com/migrant/ik/InventoryKit.java
+++ InventoryKit/com/migrant/ik/InventoryKit.java
@@ -4,15 +4,97 @@
  */
 package com.migrant.ik;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Date;
+
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Set;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import static android.content.Context.MODE_PRIVATE;
 
 public class InventoryKit {
 	
 	private static final Hashtable<String,Date> sActivatedProducts = new Hashtable<String,Date>();
 	private static final Hashtable<String,Integer> sConsumableProducts = new Hashtable<String,Integer>();
+	private static SharedPreferences sPreferences;
 
-	public static final void registerWithPaymentQueue() {
+	public static final void registerWithPaymentQueue(Activity activity) {
+		loadSettings(activity);
+	}
+	
+	public static final void loadSettings(Activity activity) {
+		Log.d("InventoryKit", "loading preferences");
+		
+		if( sPreferences==null ) {
+			Log.d("InventoryKit", "assigning preferences pointer");
+			sPreferences = activity.getPreferences(MODE_PRIVATE);
+		}
+		
+		String tProductKeys = sPreferences.getString("ActivatedProducts", "");
+		if( tProductKeys.length()>0 ) {
+			String[] tKeys = tProductKeys.split(",");
+			for (String key : tKeys) {
+				String[] kv = key.split(":");
+				sActivatedProducts.put(kv[0], new Date(kv[1]));
+			}
+		}
+
+		String tConsumableKeys = sPreferences.getString("ConsumableProducts", "");
+		if( tConsumableKeys.length()>0 ) {
+			String[] tKeys = tConsumableKeys.split(",");
+			for (String key : tKeys) {
+				String[] kv = key.split(":");
+				sConsumableProducts.put(kv[0], new Integer(kv[1]));
+			}
+		}
+	}
+	
+	public static final void saveSettings() {
+		Log.d("InventoryKit", "saving preferences");
+		
+		// save activated products
+		StringBuilder tBuilder = new StringBuilder();
+		
+		Enumeration<String> tKeys = sActivatedProducts.keys();
+		while( tKeys.hasMoreElements() ) {
+			String key = tKeys.nextElement();
+			tBuilder.append(key);
+			tBuilder.append(":");
+			tBuilder.append(sActivatedProducts.get(key));
+			tBuilder.append(",");
+		}
+		tBuilder.trimToSize();
+		String tString = tBuilder.toString();
+		if( tString==null || tString=="" ) {
+			sPreferences.edit().remove("ActivatedProducts").commit();
+		}else{
+			sPreferences.edit().putString("ActivatedProducts", tString).commit();
+		}
+		
+		// save consumable products
+		tBuilder = new StringBuilder();
+		
+		tKeys = sConsumableProducts.keys();
+		while( tKeys.hasMoreElements() ) {
+			String key = tKeys.nextElement();
+			tBuilder.append(key);
+			tBuilder.append(":");
+			tBuilder.append(sConsumableProducts.get(key));
+			tBuilder.append(",");
+		}
+		tBuilder.trimToSize();
+		tString = tBuilder.toString();
+		if( tString==null || tString=="" ) {
+			sPreferences.edit().remove("ConsumableProducts").commit();
+		}else{
+			sPreferences.edit().putString("ConsumableProducts", tString).commit();
+		}
 		
 	}
 	
@@ -35,6 +117,7 @@ public class InventoryKit {
 	 */
 	public static final void activateProduct(String productKey) {
 		sActivatedProducts.put(productKey, null);
+		saveSettings();
 	}
 	
 	/**
@@ -45,6 +128,7 @@ public class InventoryKit {
 	 */
 	public static final void activateProduct(String productKey, Date expirationDate) {
 		sActivatedProducts.put(productKey, expirationDate);
+		saveSettings();
 	}
 	
 	/**
@@ -62,6 +146,7 @@ public class InventoryKit {
 			tQuantity += quantity;
 			sConsumableProducts.put(productKey, new Integer(tQuantity));
 		}
+		saveSettings();
 	}
 	
 	/**
