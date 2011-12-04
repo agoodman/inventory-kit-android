bf3eee2ff7d8a6c466c6e073f1a0a6f015321f5d
Aubrey <aubrey.goodman@gmail.com>
2011-12-04 15:45:37 -0500

    added some basic layout components to represent state

--- IKExample/gen/com/migrant/R.java
+++ IKExample/gen/com/migrant/R.java
@@ -14,13 +14,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView1=0x7f050000;
+        public static final int TextView02=0x7f060001;
+        public static final int activationLayout=0x7f060005;
+        public static final int buyButton=0x7f060008;
+        public static final int productActivation=0x7f060007;
+        public static final int purchasedLabel=0x7f060006;
+        public static final int quantity=0x7f060003;
+        public static final int quantityLayout=0x7f060000;
+        public static final int quantityMinus=0x7f060002;
+        public static final int quantityPlus=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class raw {
+        public static final int ik_consumable_products=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int productActivated=0x7f050003;
+        public static final int purchased=0x7f050002;
+        public static final int quantity=0x7f050001;
     }
 }
--- IKExample/src/com/migrant/IKActivity.java
+++ IKExample/src/com/migrant/IKActivity.java
@@ -1,15 +1,111 @@
 package com.migrant;
 
 import android.app.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import static android.view.View.*;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.migrant.ik.InventoryKit;
 
 public class IKActivity extends Activity {
 	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
+	private static final String MY_CONSUMABLE = "your.consumable.key";
+	private static final String MY_NON_CONSUMABLE = "your.nonconsumable.key";
+	
+	private int quantityAvailable = 0;
+	private boolean productActivated = false;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// setup inventory-kit
+		InventoryKit.registerWithPaymentQueue(this);
+		
+		// quantity components
+		Button tPlusButton = (Button) findViewById(R.id.quantityPlus);
+		Button tMinusButton = (Button) findViewById(R.id.quantityMinus);
+		final TextView tQuantityValue = (TextView) findViewById(R.id.quantity);
+
+		quantityAvailable = InventoryKit.quantityAvailable(MY_CONSUMABLE);
+		tQuantityValue.setText(new Integer(quantityAvailable).toString());
+
+		tPlusButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				InventoryKit.activateProduct(MY_CONSUMABLE, 1);
+				quantityAvailable = InventoryKit.quantityAvailable(MY_CONSUMABLE);
+				tQuantityValue.setText(new Integer(quantityAvailable).toString());
+			}
+
+		});
+		
+		tMinusButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				InventoryKit.consumeQuantity(MY_CONSUMABLE, 1);
+				quantityAvailable = InventoryKit.quantityAvailable(MY_CONSUMABLE);
+				tQuantityValue.setText(new Integer(quantityAvailable).toString());
+			}
+			
+		});
+		
+		// product activation components
+		final Button tBuyButton = (Button) findViewById(R.id.buyButton);
+		final CheckBox tCheckBox = (CheckBox) findViewById(R.id.productActivation);
+
+		tBuyButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if( !productActivated ) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(IKActivity.this);
+					builder.setMessage("Are you sure you want to activate the product?")
+							.setCancelable(true)
+							.setPositiveButton("Yes",
+									new DialogInterface.OnClickListener() {
+										public void onClick(DialogInterface dialog, int id) {
+											
+											InventoryKit.activateProduct(MY_NON_CONSUMABLE);
+											productActivated = InventoryKit.productActivated(MY_NON_CONSUMABLE);
+											tCheckBox.setChecked(true);
+											tCheckBox.setText("Purchased");
+											tBuyButton.setEnabled(false);
+											tBuyButton.setVisibility(INVISIBLE);
+
+										}
+									})
+							.setNegativeButton("Cancel", 
+									new DialogInterface.OnClickListener() {
+										
+										@Override
+										public void onClick(DialogInterface dialog, int id) {
+											
+											tCheckBox.setChecked(false);
+											tCheckBox.setText("Purchased");
+											tBuyButton.setEnabled(true);
+											tBuyButton.setVisibility(VISIBLE);
+											
+										}
+										
+									});
+					AlertDialog alert = builder.create();
+					alert.show();
+				}
+			}
+			
+		});
+		
+	}
+
 }
